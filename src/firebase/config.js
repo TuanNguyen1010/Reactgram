b666064f6239f2b3bca743c4e66622acd9f8706d
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-05 13:44:30 +0100

    Chore: initlise firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage';
+import 'firebase/filestore'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB2QuQzfMz7r5oO0sytt5D-u12QYUmliH4",
+  authDomain: "reactgram-26f9e.firebaseapp.com",
+  databaseURL: "https://reactgram-26f9e.firebaseio.com",
+  projectId: "reactgram-26f9e",
+  storageBucket: "reactgram-26f9e.appspot.com",
+  messagingSenderId: "1003124477331",
+  appId: "1:1003124477331:web:18db907b4445a5a2c7ec66"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage()
+const projectFilestore = firebase.filestore()
+
+export {projectStorage, projectFilestore}
